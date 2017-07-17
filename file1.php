f0a679dc76a5a3d1ebf8502846e8e4a572715f92
Charl Joseph Mert <charl@jnz.co.za>
2017-07-17 12:04:13 +0200

    added files to test with

---
+++ file1.php
@@ -0,0 +1,8 @@
+<?php
+    echo "code before";
+
+    for ($i = 0; ($i < 10); $i++) {
+        echo "this code is everywhere. I want to remove all of it from the code base";
+    }
+
+    echo "code after";
